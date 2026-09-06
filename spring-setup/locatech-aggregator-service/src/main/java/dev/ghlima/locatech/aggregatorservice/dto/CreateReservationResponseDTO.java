a55a44940ac0af1ce24e563967b9eb9534af4c4b
Arthur Nascimento <arthur.nascimento2020@outlook.com>
2026-09-06 21:05:09 +0000

    Ajusta dto

--- spring-setup/locatech-aggregator-service/src/main/java/dev/ghlima/locatech/aggregatorservice/dto/CreateReservationResponseDTO.java
+++ spring-setup/locatech-aggregator-service/src/main/java/dev/ghlima/locatech/aggregatorservice/dto/CreateReservationResponseDTO.java
@@ -1,5 +1,8 @@
 package dev.ghlima.locatech.aggregatorservice.dto;
 
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+
+@SchemaMapping ("CreateReservationResponse")
 public record CreateReservationResponseDTO(
   Long reservationId,
   String message
